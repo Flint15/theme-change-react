7eda4f9a962192ae549e51f467fe4eefdfa73de6
Flint15 <chesspace341@gmail.com>
2026-01-07 01:12:49 +0100

    Finished the logic of Theme changing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 
 export default function App() {
+  const [text, setText] = useState("Pink");
+
   return (
     <div className="content">
-      <Button />
-      <div id="text">Pink</div>
+      <Button setText={setText}></Button>
+      <div id="text">{text}</div>
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
 import "./Button.css";
 
-export default function Button() {
+export default function Button({
+  setText,
+}: {
+  setText: (color: string) => void;
+}) {
   const changeTheme = () => {
     if (document.body.classList.contains("blue")) {
       document.body.classList.remove("blue");
+      setText("Pink");
+      setColor("aqua");
       return;
     }
     document.body.classList.add("blue");
+    setText("Blue");
+    setColor("pink");
   };
+
+  const [color, setColor] = useState("aqua");
 
   return (
     <button
@@ -20,7 +31,7 @@ export default function Button() {
         width="27px"
         height="27px"
         viewBox="0 -960 960 960"
-        fill="#ff00f2ff"
+        fill={color}
       >
         <path d="m480-120-58-52q-101-91-167-157T150-447q-39-53-54.5-97T80-634q0-94 63-157t157-63q52 0 99 22t81 62q34-40 81-62t99-22q94 0 157 63t63 157q0 46-15.5 90T810-447q-39 53-105 119T538-172l-58 52Z" />
       </svg>
